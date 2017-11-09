e1beab3722838df9170f8a2ead86fc240c7b045f
Gabo <gleyvamerino@smu.edu>
2017-11-09 13:33:06 -0600

    accelerometer added to prediction view

--- HTTPSwiftExample/PredictionViewController.swift
+++ HTTPSwiftExample/PredictionViewController.swift
@@ -8,16 +8,23 @@
 
 import Foundation
 import UIKit
+import CoreMotion
 
-class PredictionViewController: UIViewController {
+class PredictionViewController: UIViewController, URLSessionDelegate {
     
     //MARK: UI Elements
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var predictionLabel: UILabel!
     
+    let motion = CMMotionManager()
+    let motionOperationQueue = OperationQueue()
+    var ringBuffer = RingBuffer(withSize: 1000)
+    var globalMag = 0.0
+    
     override func viewDidLoad() {
         self.prepareLabel()
         self.prepareImageView()
+        self.startMotionUpdates()
     }
     
     func prepareLabel() {
@@ -29,5 +36,29 @@ class PredictionViewController: UIViewController {
         imageView.image = imageView.image!.withRenderingMode(.alwaysTemplate)
         imageView.tintColor = .newOrange
     }
+    
+    
+    // MARK: Core Motion Updates
+    func startMotionUpdates(){
+        
+        if self.motion.isDeviceMotionAvailable{
+            self.motion.deviceMotionUpdateInterval = 1.0/200
+            self.motion.startDeviceMotionUpdates(to: motionOperationQueue, withHandler: self.handleMotion )
+        }
+    }
+    
+    func handleMotion(_ motionData:CMDeviceMotion?, error:Error?){
+        if let accel = motionData?.userAcceleration {
+            // Send the data from here
+            
+            self.ringBuffer.addNewData(xData: accel.x, yData: accel.y, zData: accel.z)
+            let mag = fabs(accel.x)+fabs(accel.y)+fabs(accel.z)
+            globalMag = mag
+            print("Mag: ", mag)
+        }
+    }
+    
+
+    
     
 }
